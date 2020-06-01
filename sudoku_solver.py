6c476e2222bf6d8423f00ed835575a97ce49bf46
SanjayS96 <sanjaysawh96@gmail.com>
2020-06-01 00:25:03 -0400

    if name == main

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -2,9 +2,10 @@ import numpy as np
 from modules import *
 from modules import control
 
-board_stuff.save()
-board, npboard = board_stuff.get_board()
-solved_board = solver.solve_it(board)
-solved_npboard = np.asarray(board)
-changes_board = solved_npboard - npboard
-control.controller(changes_board)
+if __name__ == "__main__":
+    board_stuff.save()
+    board, npboard = board_stuff.get_board()
+    solved_board = solver.solve_it(board)
+    solved_npboard = np.asarray(board)
+    changes_board = solved_npboard - npboard
+    control.controller(changes_board)
