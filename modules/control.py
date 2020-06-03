972e9a86b40d53e9290fd824608a2668f21eb65c
SanjayS96 <sanjaysawh96@gmail.com>
2020-06-03 08:05:53 -0400

    increased speed of solver, stable

--- modules/control.py
+++ modules/control.py
@@ -7,7 +7,7 @@ def controller(c_board):
 
     def click():
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 948,372)
-        time.sleep(0.1)
+        time.sleep(0.05)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 948,372)
     
     mouse_x = np.array([390, 445, 500, 555, 610, 665, 720, 775, 830])
@@ -20,9 +20,9 @@ def controller(c_board):
         x = nonzero_x[i]
         y = nonzero_y[i]
         
-        time.sleep(0.1)
+        time.sleep(0.05)
         win32api.SetCursorPos((mouse_x[x],mouse_y[y]))
-        time.sleep(0.1)
+        time.sleep(0.05)
         click()
 
         answer = c_board[y][x]
