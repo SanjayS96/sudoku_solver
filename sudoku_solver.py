5e61d1f333fd1cf4626e9ec33cbddc14e8cf987e
SanjayS96 <sanjaysawh96@gmail.com>
2020-06-19 13:55:53 -0400

    fixed module import

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     print(title_screen)
     print('   Press enter to begin...', end='')
     input()
-    board_stuff.save()
-    board, npboard = board_stuff.get_board()
+    board_data.save()
+    board, npboard = board_data.get_board()
     solved_board = solver.solve_it(board)
     solved_npboard = np.asarray(board)
     changes_board = solved_npboard - npboard
